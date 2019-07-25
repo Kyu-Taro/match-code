25d725f9489735cc5b8c1f2f87eba5a2b7df9fd0
Kyu Taro <kyutaro@Kyu.local>
2019-07-25 20:31:27 +0900

    header作成中

--- php/index.php
+++ php/index.php
@@ -1,3 +1,24 @@
-<?php
-    echo 'e';
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/index.css" type="text/css">
+    <title>Match-Code</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="../img/logo.png" alt="ロゴ"></a>
+        <nav class="header-nav">
+            <ul>
+                <li><a href="register-view.php">Regist</a></li>
+                <li><a href="login-view.php">Login</a></li>
+                <li><a href="post-view.php">Post</a></li>
+                <li><a href="teams-view.php">Team</a></li>
+                <li><a href="detail.html">Detail</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
