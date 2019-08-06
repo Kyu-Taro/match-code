768dfcf483eadbdaec88cf97f67d48b01181351c
Kyu Taro <kyutaro@Kyu.local>
2019-08-06 21:26:51 +0900

    commit

--- php/detail.php
+++ php/detail.php
@@ -5,7 +5,7 @@ require('../php/function.php');
     $url2="login-view.php";
     $url3="post-view.php";
     $url4="team-view.php";
-    $url5="../html/detail.html";
+    $url5="detail.php";
     $url6="myPage-view.php";
     $link1="Regist";
     $link2="Login";
